a477aa5a4a9ae15da6e3c0ebc714ad2da6a23b83
shane tully <shane@shanetully.com>
2013-12-04 12:19:23 -0500

    check for internet connection

--- iCATA/SPTAppDelegate.m
+++ iCATA/SPTAppDelegate.m
@@ -6,8 +6,9 @@
 //  Copyright (c) 2013 shane. All rights reserved.
 //
 
-#import "SPTAppDelegate.h"
 #import <GoogleMaps/GoogleMaps.h>
+#import "SPTAppDelegate.h"
+#import "Reachability.h"
 
 @implementation SPTAppDelegate
 
@@ -20,6 +21,18 @@
     NSString *defaultPrefsFile = [[NSBundle mainBundle] pathForResource:@"defaultPrefs" ofType:@"plist"];
     NSDictionary *defaultPreferences = [NSDictionary dictionaryWithContentsOfFile:defaultPrefsFile];
     [prefs registerDefaults:defaultPreferences];
+    
+    // Check for an active internet connection even though Google Maps apparently crashes the app if there is no connection
+    // At least a warning dialog will tell the user that something isn't right
+    Reachability *networkReachability = [Reachability reachabilityForInternetConnection];
+    NetworkStatus networkStatus = [networkReachability currentReachabilityStatus];
+    if (networkStatus == NotReachable) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No internet connection"
+                                                        message:@"An active internet connection is necessary to retrieve bus information, but none was found."
+                                                       delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil, nil];
+        
+        [alert show];
+    }        
     
     return YES;
 }
